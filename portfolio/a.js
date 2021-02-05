9f0b33050c84fb3c73c5b553d12eb73e233619d9
auddy <soumyadeepauddy1999@gmail.com>
2021-02-05 10:45:08 +0530

    added navbar

--- portfolio/a.js
+++ portfolio/a.js
@@ -3,6 +3,12 @@ $("#main-content").slideDown(2000);
 
 window.addEventListener('scroll',function(){
 	let scroll = window.pageYOffset;
+	if(scroll>200){
+		$('#nav').addClass('navScroll');
+	}
+	else{
+		$('#nav').removeClass('navScroll');
+	}
 	$('.scrolled').each(function(){
 		$(this).css('transform','translateY(-'+parseInt($(this).attr('speed'))*scroll+'px)')		
 		$(this).css('opacity',1-0.003*scroll)
