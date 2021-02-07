702955fdb4a9834e1c1613b077f91f19b3a791c9
auddy <soumyadeepauddy1999@gmail.com>
2021-02-07 17:47:52 +0530

    skill network animation

--- portfolio/a.js
+++ portfolio/a.js
@@ -1,17 +1,62 @@
+var sX = 0
+var sY = 1500
+$("#skills h1").css("display","none")
+$("#skills h1:nth-child(1)").css("display","block")
+$("#skills h1").each(function(){
+	$(this).css({'top':sY+parseInt($(this).attr('y'))+'px','left':sX+parseInt($(this).attr('x'))+'px'})
+})
 
 $("#main-content").slideDown(2000);
 
 window.addEventListener('scroll',function(){
 	let scroll = window.pageYOffset;
-	if(scroll>200){
+	console.log(scroll)
+	if(scroll > 200){
 		$('#nav').addClass('navScroll');
 	}
 	else{
 		$('#nav').removeClass('navScroll');
 	}
+
+	if(scroll > 900){
+		skillStart = 1
+		setTimeout(function(){
+			$("#skills h1:nth-child(2)").fadeIn(1000)
+		},2700)
+		setTimeout(function(){
+			$("#skills h1:nth-child(3)").fadeIn(1000)
+			$("#skills h1:nth-child(6)").fadeIn(1000)
+		},4400)
+		setTimeout(function(){
+			$("#skills h1:nth-child(4)").fadeIn(1000)
+			$("#skills h1:nth-child(7)").fadeIn(1000)
+			$("#skills h1:nth-child(9)").fadeIn(1000)
+		},6100)
+		setTimeout(function(){
+			$("#skills h1:nth-child(5)").fadeIn(1000)
+			$("#skills h1:nth-child(8)").fadeIn(1000)
+			$("#skills h1:nth-child(10)").fadeIn(1000)
+			$("#skills h1:nth-child(12)").fadeIn(1000)
+		},7900)
+		setTimeout(function(){
+			$("#skills h1:nth-child(11)").fadeIn(1000)
+			$("#skills h1:nth-child(13)").fadeIn(1000)
+			$("#skills h1:nth-child(15)").fadeIn(1000)
+		},9700)
+		setTimeout(function(){
+			$("#skills h1:nth-child(14)").fadeIn(1000)
+			$("#skills h1:nth-child(16)").fadeIn(1000)
+		},11400)
+		setTimeout(function(){
+			$("#skills h1:nth-child(17)").fadeIn(1000)
+		},13100)
+		
+	}
+
 	$('.scrolled').each(function(){
 		$(this).css('transform','translateY(-'+parseInt($(this).attr('speed'))*scroll+'px)')		
 		$(this).css('opacity',1-0.003*scroll)
 	})
 	
 })
+
--- portfolio/network.js
+++ portfolio/network.js
@@ -8,62 +8,58 @@ var circleSize = 25
 var movingCircleSize = 10
 var moveSpeed = 0.05
 var strokeColor = 'white'
+var target = '#skills'
+var skillStart = 0;
 
 function setup() {
   wd = window.innerWidth
   ht = window.innerHeight
-  ctc = createCanvas(wd, 1.5*ht);
+  ctc = createCanvas(0.7*wd, 1.2*ht);
+  // ctc = $('#defaultCanvas0')
   ctc.drawingContext.fillStyle = strokeColor
 
   network = new Network(width/2, height/2);
   
-  x=-400;y=-420
+  x=-350; y=-400
   var a = new Neuron(x, y);
   var b = new Neuron(x, y+200);
   var c = new Neuron(x, y+400);
   var d = new Neuron(x, y+600);
   var e = new Neuron(x, y+800);
-  var b1 = new Neuron(x+300, y+200);
-  var b2 = new Neuron(x+600, y+200);
-  var b3 = new Neuron(x+900, y+200);
-  var c1 = new Neuron(x+300, y+400);
-  var c2 = new Neuron(x+600, y+400);
-  var c3 = new Neuron(x+900, y+400);
-  var d1 = new Neuron(x+300, y+600);
-  var d2 = new Neuron(x+600, y+600);
-  var d3 = new Neuron(x+900, y+600);
-  var e1 = new Neuron(x+300, y+800);
-  var e2 = new Neuron(x+600, y+800);
-  var e3 = new Neuron(x+900, y+800);
+  var b1 = new Neuron(x+250, y+200);
+  var b2 = new Neuron(x+500, y+200);
+  var b3 = new Neuron(x+750, y+200);
+  var c1 = new Neuron(x+250, y+400);
+  var c2 = new Neuron(x+500, y+400);
+  var c3 = new Neuron(x+750, y+400);
+  var d1 = new Neuron(x+250, y+600);
+  var d2 = new Neuron(x+500, y+600);
+  var d3 = new Neuron(x+750, y+600);
+  var e1 = new Neuron(x+250, y+800);
+  var e2 = new Neuron(x+500, y+800);
+  var e3 = new Neuron(x+750, y+800);
   
   //skill
   network.connect(a , b , 1);//Data Science
   network.connect(b , c , 1);//Coding
   network.connect(c , d , 1);//Web dev
   network.connect(d , e , 1);//Others
-    
   //Data Science
   network.connect(b , b1 , 1);//Python
   network.connect(b1 , b2 , 1);//ML-NLP
   network.connect(b2 , b3 , 1);//Deep Learning
-  
   //Coding
   network.connect(c , c1 , 1);//Competitive Coding
   network.connect(c1 , c2 , 1);//DS and Algorithmn
   network.connect(c2 , c3 , 1);//C/C++
-  
   //Web dev
   network.connect(d , d1 , 1);//HTML-CSS-JS
   network.connect(d1 , d2 , 1);//PHP
   network.connect(d2 , d3 , 1);//JQuery-Bootstrap
-  
   //Others
   network.connect(e , e1 , 1);//Heroku
   network.connect(e1 , e2 , 1);//Github
   network.connect(e2 , e3 , 1);//Networking
-  
-  
-  
   
   network.addNeuron(a);
   network.addNeuron(b);
@@ -83,6 +79,9 @@ function setup() {
   network.addNeuron(e2);
   network.addNeuron(e3);
   
+  //UNCOMMENT THESE TO USE TARGET
+  canv = $("#defaultCanvas0")
+  $(target).append(canv)
 }
 
 function draw() {
@@ -91,7 +90,7 @@ function draw() {
   network.update();
   network.display();
   
-  if (frameCount % 30 == 0) {
+  if (frameCount%30==0 && skillStart==1) {
     network.feedforward(1); //originate speed
   }
 }
@@ -156,6 +155,7 @@ function Network(x, y) {
   
   this.feedforward = function(input) {
     ncount++;
+
     if(ncount%interval != 0 && ncount%interval!=1){
       // console.log(ncount);
       return;
@@ -223,3 +223,4 @@ function Neuron(x, y) {
     // console.log(this.r)
   }
 }
+
